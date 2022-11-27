87be773dc84de4441f8502d52fe281bb14ce33df
SalimmReza <salimnuaa@gmail.com>
2022-11-27 12:50:45 +0600

    paid shob jaigai accessed

--- src/Home/CategoryDetails/CategoryDetails2.js
+++ src/Home/CategoryDetails/CategoryDetails2.js
@@ -4,7 +4,7 @@ import Button from '../../Components/Button/Button';
 
 const CategoryDetails2 = ({ CD, setDetails }) => {
 
-    // console.log(CD);
+    console.log(CD);
     const { category_id,
         category_name,
         image,
@@ -14,45 +14,52 @@ const CategoryDetails2 = ({ CD, setDetails }) => {
         original_price,
         year_of_use,
         time_posted,
+        advertise,
         sellers_name } = CD;
 
-    console.log();
+    // console.log();
 
 
 
     return (
         <div>
 
-            <div className="card bg-slate-100 shadow-xl">
-                <figure><img
-                    className='h-[350px] w-full'
-                    src={image} alt="" /></figure>
-                <div className="card-body text-black">
-                    <h2 className="card-title">
-                        {item_name}
-                        <div className="badge badge-secondary">Years Used : <span className='text-red-500 font-bold text-2xl'>{year_of_use}</span> </div>
-                    </h2>
-                    {/* <p>{email.email}</p> */}
-                    <p className='font-bold'>Sellers Name: {sellers_name}</p>
-                    <p className='font-bold'>Location: {sellers_location}</p>
-                    <p className='font-bold'>Time Posted: {time_posted}</p>
-                    <div className="card-actions justify-end mt-4">
-                        <div className="badge badge-outline">Original Price: $  {original_price}</div>
-                        <div className="badge badge-outline">Resale Price: $  {resale_price}</div>
+            {
+
+                CD?.paid ? "" :
+
+
+                    < div className="card bg-slate-100 shadow-xl">
+                        <figure><img
+                            className='h-[350px] w-full'
+                            src={image} alt="" /></figure>
+                        <div className="card-body text-black">
+                            <h2 className="card-title">
+                                {item_name}
+                                <div className="badge badge-secondary">Years Used : <span className='text-red-500 font-bold text-2xl'>{year_of_use}</span> </div>
+                            </h2>
+                            {/* <p>{email.email}</p> */}
+                            <p className='font-bold'>Sellers Name: {sellers_name}</p>
+                            <p className='font-bold'>Location: {sellers_location}</p>
+                            <p className='font-bold'>Time Posted: {time_posted}</p>
+                            <div className="card-actions justify-end mt-4">
+                                <div className="badge badge-outline">Original Price: $  {original_price}</div>
+                                <div className="badge badge-outline">Resale Price: $  {resale_price}</div>
+                            </div>
+
+
+                            <Button
+                                classes='px-10 mt-6'
+                            > <label
+                                onClick={() => setDetails(CD)}
+                                htmlFor="book-nowModal"
+                            >Book Now</label></Button>
+                        </div>
                     </div>
+            }
 
 
-                    <Button
-                        classes='px-10 mt-6'
-                    > <label
-                        onClick={() => setDetails(CD)}
-                        htmlFor="book-nowModal"
-                    >Book Now</label></Button>
-                </div>
-            </div>
-
-
-        </div>
+        </div >
     );
 };
 
